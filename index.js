f070c031ce71713778f89b3bf5d20c3309a5ae9a
Harsh Singh <harsh@Harshs-MacBook-Pro.local>
2022-04-10 01:13:28 +0530

    Deploying

--- index.js
+++ index.js
@@ -129,13 +129,8 @@ app.post("/delete", (req, res)=>{
         })
     }
 
-})
+});
 
-let port = process.env.PORT;
-if (port == null || port == ""){
-port = 3000;
-}
-
-app.listen(port, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is started successfully.")
 });
